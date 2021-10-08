5cddb9050675d5ade6b06b1599effe33d8f2aed3
Jonas <jonas.ries@gmail.com>
2021-10-08 13:58:16 +0200

    Add gpu load, make only training pids killable

--- dispatch/main.py
+++ dispatch/main.py
@@ -1,42 +1,83 @@
 import subprocess
 import psutil
 import os
+from pathlib import Path
 from typing import Dict, List, Optional
 
-from fastapi import FastAPI
+import torch
+import GPUtil
+from fastapi import FastAPI, HTTPException
+
 
 app = FastAPI()
 
-watch_folder = "/mnt/t2ries/DECODE"
 decode_default = "/home/riesgroup/xconda3/envs/decode_v010/bin/python"
+
+watch_dir = Path("/mnt/t2ries/decode/experiments")
+log_dir = Path("/mnt/t2ries/decode/log")
+pid_pool = set()
+
 
 @app.get("/status")
 async def status() -> Dict[str, str]:
     return {
         "health": "Dispatcher alive.",
-        "watch_folder": watch_folder,
+        "watch_dir": str(watch_dir),
+        "log_dir": str(log_dir),
     }
 
 
 @app.get("/status_gpus")
 async def status_gpu() -> Dict[str, str]:
-    return {"cuda:0": "100% util, 10GB mem"}
+    n = torch.cuda.device_count()
+
+    out = dict()
+    for ix in range(n):
+        id_gpu = f"cuda:{ix}"
+        out.update({
+            id_gpu: {
+                "device_name": torch.cuda.get_device_name(id_gpu),
+                "memory_util": GPUtil.getGPUs()[ix].memoryUsed,
+                "memory_total": GPUtil.getGPUs()[ix].memoryTotal,
+                "load": GPUtil.getGPUs()[ix].load,
+                "temperature": GPUtil.getGPUs()[ix].temperature,
+            }
+        })
+    return out
 
 
 @app.get("/envs")
 async def envs() -> List[str]:
-    decode_envs = ["/home/AD/muellelu/xconda3/bin/python"]
+    decode_envs = [decode_default]
     return decode_envs
 
 
 @app.post("/submit")
-async def submit(path: str) -> int:
-    p = subprocess.Popen([decode_default, "-m", "decode.neuralfitter.train.live_engine", "-p", f"{watch_folder}/{path}"])
+async def submit(path_param: Path) -> int:
+
+    p = subprocess.Popen([
+        decode_default,
+        "-m",
+        "decode.neuralfitter.train.live_engine",
+        "-p",
+        f"{str(watch_dir)}/{str(path_param)}",
+        "-l",
+        f"{str(log_dir)}",
+        ])
     pid = p.pid
+
+    # bookkeeping
+    pid_pool.add(pid)
+
     return pid
 
 
 @app.post("/kill")
 async def kill(pid: int):
+    if not pid in pid_pool:
+        raise HTTPException(status_code=400, detail="Can not kill process that is not a training.")
+
     p = psutil.Process(pid)
     p.terminate()
+
+    pid_pool.remove(pid)
